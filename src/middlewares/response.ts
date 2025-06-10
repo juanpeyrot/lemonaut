7a2d0e2a5b85dd3e895c53ca4face2489ba79a07
unknown <juanpeyrot15@gmail.com>
2025-06-10 10:30:51 -0300

    res render

--- src/middlewares/response.ts
+++ src/middlewares/response.ts
@@ -1,5 +1,7 @@
 import { ServerResponse } from "http";
 import { IncomingMessage } from "http";
+import ejs from "ejs";
+import path from "path";
 
 type NextFunction = () => void;
 
@@ -15,8 +17,7 @@ const ResponseDecorator = (
   res: ServerResponse,
   next: NextFunction
 ): void => {
-
-	const customRes: DecoratedResponse = res as DecoratedResponse;
+  const customRes: DecoratedResponse = res as DecoratedResponse;
 
   customRes.status = (status: number): DecoratedResponse => {
     customRes.statusCode = status;
@@ -33,8 +34,16 @@ const ResponseDecorator = (
   };
 
   customRes.render = async (templatePath: string, data: any): Promise<void> => {
-
-  }
+    try {
+      const fullPath = path.join(process.cwd(), "views", templatePath);
+      const html = await ejs.renderFile(fullPath, data, { async: true });
+      customRes.setHeader("Content-Type", "text/html");
+      customRes.end(html);
+    } catch (err) {
+      customRes.statusCode = 500;
+      customRes.end("Template rendering error");
+    }
+  };
 
   next();
 };
